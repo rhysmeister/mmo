eac0d3934e29057beefe5201ef4ae37590ecff78
Rhys Campbell <rhys.james.campbell@googlemail.com>
2016-04-02 19:05:48 +0200

    Added further command-line options

--- python/app/mm.py
+++ python/app/mm.py
@@ -13,13 +13,14 @@ import inspect
 import os
 import sys
 import argparse
-
+import time
+from curses import wrapper
 
 from bgcolours import bgcolours
 
 execfile(os.path.dirname(os.path.abspath(inspect.stack()[0][1]))  + "/../pymmo/pymmo.py")
 
-def display_cluster_state(mmo):
+def display_cluster_state(mmo, c):
     """
     Print out a overview of the MongoDB Cluster and its status. The inital connection should be a mongos for this to work correctly.
     :param self:
@@ -40,9 +41,9 @@ def display_cluster_state(mmo):
 
 
 def print_replication_summary(replication_summary):
-    print(bgcolours.BOLD + "{:<30} {:<10} {:<10} {:<10} {:<10} {:<10}".format("hostname", "replicaset", "state", "configV", "uptime", "slaveDelay") + bgcolours.ENDC)
+    sys.stdout.write(bgcolours.BOLD + "{:<30} {:<10} {:<10} {:<10} {:<10} {:<10}\n".format("hostname", "replicaset", "state", "configV", "uptime", "slaveDelay") + bgcolours.ENDC)
     for host in replication_summary:
-        print(bgcolours.OKGREEN + "{:<30} {:<10} {:<10} {:<10} {:<10} {:<10}".format(host["hostname"], host["replicaset"], host["state"], host["configVersion"], host["uptime"], host["slaveDelay"]) + bgcolours.ENDC)
+        sys.stdout.write(bgcolours.OKGREEN + "{:<30} {:<10} {:<10} {:<10} {:<10} {:<10}\n".format(host["hostname"], host["replicaset"], host["state"], host["configVersion"], host["uptime"], host["slaveDelay"]) + bgcolours.ENDC)
 
 
 def print_list_of_hosts(title, host_list, colour):
@@ -52,11 +53,11 @@ def print_list_of_hosts(title, host_list, colour):
     :param host_list: [ { "hostname": <string>, "port": <int> }, ...]
     :return:
     """
-    print(bgcolours.BOLD + title + bgcolours.ENDC)
+    sys.stdout.write(bgcolours.BOLD + title + bgcolours.ENDC + "\n")
     print_count = 0
     for host in host_list:
         if print_count % 3 == 0 and print_count > 0:  # max 3 hosts per line
-            print("")
+            sys.stdout.write("\n")
         sys.stdout.write(colour + "{0}:{1}      ".format(host["hostname"], host["port"]) + bgcolours.ENDC)
         print_count+=1
     print("")
@@ -67,24 +68,35 @@ MAIN SECTION STARTS HERE
 parser = argparse.ArgumentParser(description='MongoDB Manager')
 parser.add_argument('--summary', action='store_true', help='Show a summary of the MongoDB Cluster Topology')
 parser.add_argument('--repl', action='store_true', help='Show a summary of the replicaset state')
+parser.add_argument('--show_all', action='store_true', help='Show all information screens')
 parser.add_argument("-H", "--mongo_hostname", type=str, default="localhost", required=False, help="Hostname for the MongoDB mongos process to connect to")
 parser.add_argument("-P", "--mongo_port", type=int, default=27017, required=False, help="Port for the MongoDB mongos process to connect to")
 parser.add_argument("-u", "--mongo_username", type=str, default="admin", required=False, help="MongoDB username")
 parser.add_argument("-p", "--mongo_password", type=str, default="admin", required=False, help="MongoDB password")
 parser.add_argument("-D", "--mongo_auth_db", type=str, default="admin", required=False, help="MongoDB authentication database")
+parser.add_argument("-r", "--repeat", type=int, default=1, required=False, help="Repeat the action N number of times")
+parser.add_argument("-i", "--interval", type=int, default=2, required=False, help="Number of seconds between each repeat")
 args = parser.parse_args()
 
-# Just connect to the default host for now. Options to come later
+###################################################
+# Main program starts here
+###################################################
+
 mmo = MmoMongoCluster(args.mongo_hostname, args.mongo_port, args.mongo_username, args.mongo_password, args.mongo_auth_db)
 c = mmo.mmo_connect()
 if c:
-    if args.summary:
-        display_cluster_state(mmo)
-    if args.repl:
-        rs = mmo.mmo_replication_status_summary(c)
-        print_replication_summary(rs)
-else:
-    exit(1)
+    while args.repeat != 0:
+        if args.summary or args.show_all:
+            display_cluster_state(mmo, c)
+        if args.repl or args.show_all:
+            rs = mmo.mmo_replication_status_summary(c)
+            print_replication_summary(rs)
+        args.repeat -= 1
+        if args.repeat > 0:
+            time.sleep(args.interval)
+            sys.stdout.flush()
+    else:
+        exit(1)
 
 
 
